cd72b7327e76256dae5a7c573c103c58ef7bba1d
Zemorath <trentward10@gmail.com>
2023-05-18 22:43:37 -0500

    TaskLister Project Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
+class TaskList {
+  constructor() {
+    this.tasks = [];
+  }
+
+  createNewTask(description) {
+    const newTask = new Task(description);
+    this.tasks.push(newTask);
+  }
+
+  renderTasks() {
+    return this.tasks.map((task) => task.render()).join("");
+  }
+
+  deleteTask(description) {
+    this.tasks = this.tasks.filter((task) => task.description !== description);
+  }
+}
+
+class Task {
+  constructor(description) {
+    this.description = description;
+  }
+
+  render() {
+    return `
+      <li>
+        ${this.description}
+        <button data-description="${this.description}">X</button>
+      </li>
+      `;
+  }
+}
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const taskList = new TaskList();
+  const taskItem = document.getElementById("tasks");
+  const submitNewTask = document.getElementById("create-task-form");
+  const taskDescription = document.getElementById("new-task-description");
+
+  const renderApp = () => (taskItem.innerHTML = taskList.renderTasks());
+
+  submitNewTask.addEventListener("submit", (x) => {
+    x.preventDefault();
+    taskList.createNewTask(taskDescription.value);
+    x.target.reset();
+    renderApp();
+  });
+
+  taskItem.addEventListener("click", (x) => {
+    if (x.target.nodeName === "BUTTON") {
+      taskList.deleteTask(x.target.dataset.description);
+      renderApp();
+    }
+  });
+
 });
+
+
+
